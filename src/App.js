d58650f7e5dbfa814737428c987ca5bf9a2e806b
CristoCruz007 <cristocruz2000@gmail.com>
2024-05-23 13:16:58 -0700

    segundo respo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Layout from "./app/layout/Layout";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Inicio from "./Inicio/Inicio";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        {/* Renderiza tanto Inicio como Layout */}
+                        <Route index element={<Inicio />} />
+                    </Route>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
-
-export default App;
